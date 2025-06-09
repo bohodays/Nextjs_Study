21fba4b4dc02fa6d710c06986701eff752230023
bohodays <bohodays0316@gmail.com>
2025-06-10 00:25:57 +0900

    feat: Page Router 소개

---
+++ onebite-next/section02/src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+/* next앱의 html을 설정하는 컴포넌트 / React의 index.html과 동일한 역할 */
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
