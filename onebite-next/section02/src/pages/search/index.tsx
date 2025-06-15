adfd44339c788a50701c3d383021f5d7e247a263
bohodays <bohodays0316@gmail.com>
2025-06-15 22:52:54 +0900

    feat: 페이지별 레이아웃 설정하기

--- onebite-next/section02/src/pages/search/index.tsx
+++ onebite-next/section02/src/pages/search/index.tsx
@@ -1,10 +1,16 @@
+import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
 const Page = () => {
   const router = useRouter();
   const { q } = router.query;
 
   return <h1>search {q}</h1>;
+};
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
 };
 
 export default Page;
