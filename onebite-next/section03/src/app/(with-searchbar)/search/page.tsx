865c922537905815e77f7526cc7f7e33d96f8d3f
bohodays <bohodays0316@gmail.com>
2025-07-02 01:11:05 +0900

    feat: 앱 라우터의 데이터 패칭

--- onebite-next/section03/src/app/(with-searchbar)/search/page.tsx
+++ onebite-next/section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,5 @@
 import BookItem from "@/app/components/book-item";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
 
 export default async function Page({
   searchParams,
@@ -7,6 +7,13 @@ export default async function Page({
   searchParams: Promise<{ q?: string }>;
 }) {
   const { q } = await searchParams;
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`
+  );
+  if (!response.ok) return <div>오류가 발생했습니다 ...</div>;
+
+  const books: BookData[] = await response.json();
 
   return (
     <div>
