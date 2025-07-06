c3375b2444049ffc64600c04fbd57a8394672250
bohodays <bohodays0316@gmail.com>
2025-07-06 15:58:48 +0900

    feat: 풀 라우터 캐시 3. 동적 경로에 적용하기

--- onebite-next/section03/src/app/(with-searchbar)/search/page.tsx
+++ onebite-next/section03/src/app/(with-searchbar)/search/page.tsx
@@ -6,10 +6,13 @@ export default async function Page({
 }: {
   searchParams: Promise<{ q?: string }>;
 }) {
+  // Search 페이지는 searchParams와 같은 동적 함수로 의존하기 때문에 static 페이지로 설정할 수 없음
   const { q } = await searchParams;
 
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    // 데이터를 캐싱하는 방향으로 최적화 선택
+    { cache: "force-cache" }
   );
   if (!response.ok) return <div>오류가 발생했습니다 ...</div>;
 
--- onebite-next/section03/src/app/book/[id]/page.tsx
+++ onebite-next/section03/src/app/book/[id]/page.tsx
@@ -1,5 +1,22 @@
 import { BookData } from "@/types";
 import style from "./page.module.css";
+import { notFound } from "next/navigation";
+
+/**
+ * generateStaticParams로 설정된 id 1,2,3 값 외에는 404 페이지로 이동시킴
+ */
+// export const dynamicParams = false;
+
+/**
+ * 정적인 파라미터를 생성하는 함수
+ * 약속된 함수
+ */
+export function generateStaticParams() {
+  // Next 서버가 build time에 정적 파라미터를 읽어서 해당하는 북페이지를 정적으로 만듦
+  // 문자열 데이터만 가능함
+  // 페이지 라우터의 getStaticPath와 동일한 역할을 함
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
 
 export default async function Page({
   params,
@@ -11,7 +28,12 @@ export default async function Page({
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
   );
-  if (!response.ok) return <div>오류가 발생했습니다 ...</div>;
+  if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
+    }
+    return <div>오류가 발생했습니다 ...</div>;
+  }
 
   const book: BookData = await response.json();
 
