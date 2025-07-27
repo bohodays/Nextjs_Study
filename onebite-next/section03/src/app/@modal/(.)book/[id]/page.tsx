b3784b168be357845cd9d649faf6a6371af28057
bohodays <bohodays0316@gmail.com>
2025-07-27 22:36:51 +0900

    feat: 병합 & 인터셉팅 라우트

--- onebite-next/section03/src/app/@modal/(.)book/[id]/page.tsx
+++ onebite-next/section03/src/app/@modal/(.)book/[id]/page.tsx
@@ -4,12 +4,9 @@ import Modal from "@/app/components/modal";
 
 const Page = (props: any) => {
   return (
-    <div>
-      가로채기 성공!
-      <Modal>
-        <BookPage {...props} />
-      </Modal>
-    </div>
+    <Modal>
+      <BookPage {...props} />
+    </Modal>
   );
 };
 
